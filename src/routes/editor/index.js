a46bedcbaa6048c2e6747b8bd531c214605db3a2
coding-nerfilio <frisso2001@gmail.com>
2021-06-04 15:20:03 -0300

    EditorRoute now checks if brainstorm already exists

--- src/routes/editor/index.js
+++ src/routes/editor/index.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useHistory, useParams } from "react-router";
+
+import BrainStorm from "../../logic/BrainStorm";
+
 const EditorRoute = () => {
+	const { id } = useParams();
+	const history = useHistory();
+
+	//Check if brainstorm exists
+	useEffect(() => {
+		let bs = new BrainStorm(id);
+		bs.itExists().then((e) => !e && history.push("/invalid"));
+	}, []);
+
 	return <div>EditorRoute</div>;
 };
 export default EditorRoute;
