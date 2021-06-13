6e430d2738779092084ce9efda18d51bdf575368
coding-nerfilio <frisso2001@gmail.com>
2021-06-13 20:45:01 -0300

    Add icon to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,11 @@
 import "./style.css";
+import icon from "../../assets/Icon.svg";
 const Header = () => {
-	return <div className="header">Header</div>;
+	return (
+		<div className="header">
+			<img className="icon" src={icon} />
+		</div>
+	);
 };
 
 export default Header;
